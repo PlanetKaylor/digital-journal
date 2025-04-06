94e3ce0a6af30627e56bb06bf731723515c44210
Kaylor Davis <74217234+PlanetKaylor@users.noreply.github.com>
2025-04-06 12:57:10 -0700

    Update homepage

--- digital-journal-app/src/pages/Homepage.jsx
+++ digital-journal-app/src/pages/Homepage.jsx
@@ -1,10 +1,17 @@
 import notebook from '../assets/notebook-paper/1.png'
-import beigePostIt from '../assets/stickers/top-right-beige-paper.png'
+import beigePostItRight from '../assets/stickers/top-right-beige-paper.png'
+import beigePostItLeft from '../assets/stickers/top-left-beige-paper.png'
+import gridPostIt from '../assets/stickers/top-left-grid-paper.png'
 
 function Homepage() {
     return(
-          <div class="relative h-screen w-full bg-cover bg-center"  style={{ backgroundImage: `url(${notebook})`}}>
-                 <img src={beigePostIt} alt="Post-it 1" class="absolute -top-[400px] -right-[80px] w-[1080px] h-auto z-10 rotate-[3deg]"></img>
+          <div className="relative h-screen w-full bg-cover bg-center overflow-hidden"  style={{ backgroundImage: `url(${notebook})`}}>
+                <h1 className="font-notebook text-4xl"> My journal header with a custom font ✍️</h1>    
+
+                 <img src={beigePostItRight} alt="post-it-right" className="absolute -top-[640px] -right-[120px] w-[1600px] h-auto z-10 rotate-[0deg]"></img>
+                 <img src={gridPostIt} alt="grid-post-it" className="absolute -top-[400px] -left-[280px] w-[1080px] h-auto z-10"></img>
+                 <img src={beigePostItLeft} alt="post-it-left" className="absolute -bottom-[900px] -left-[280px] w-[1680px] h-auto z-0"></img>  
+
           </div>
     );
 }
